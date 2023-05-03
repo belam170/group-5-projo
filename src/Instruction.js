769ab8d6663469d0b3fc1200e467380b64c00e95
belammuia <belammuia0@gmail.com>
2023-05-03 20:56:59 +0300

    branch

---
+++ src/Instruction.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ function Instruction(props) {
+  return (
+    <div>
+        {props.Instructions}
+    </div>
+  )
+}
+export default Instruction;
--- src/Searchbar.js
+++ src/Searchbar.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Searchbar() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+  const [instructions, setInstructions] = useState(false)
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+        .then(res => res.json())
+        .then(data => setSearchResults(data.meals))
+        .catch(error => console.error(error))
+    }
+  }, [searchTerm])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setSearchTerm(e.target.elements.search.value)
+  }
+
+  function handleInstructionsClick() {
+    setInstructions(!instructions)
+  }
+
   return (
     <div>
-    <hi>hi there</hi>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="search" id="add"/>
+        <button type="submit" id="but">Search</button>
+      </form>
+      
+      
+      {searchResults.map(meal => (
+        <div key={meal.idMeal} className="card">
+          <p>{meal.strMeal}</p>
+          <img src={meal.strMealThumb} alt="unavailable" />
+          <button onClick={handleInstructionsClick}>Get Recipe</button>
+          {instructions && (
+            <div className="box">
+              <h3>Instructions:</h3>
+              <p>{meal.strInstructions}</p>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
+    
   )
 }
