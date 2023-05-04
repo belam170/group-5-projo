f8508ea9cab87d5c8c524d37ddf3d25e020f6176
belammuia <belammuia0@gmail.com>
2023-05-04 10:40:11 +0300

    About

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./Header";
 import Searchbar from "./Searchbar";
-
+import About from "./About"
 function App() {
   return (
     <div>
       <Header />
       <Searchbar />
+      <About />
     </div>
   );
 }
