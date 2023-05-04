00fa6a1176e4edb95a989c8db9715d370bac6280
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-05-04 11:12:37 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./Header";
 import Searchbar from "./Searchbar";
-import About from "./About"
+import About from "./About";
+import Footer from "./Footer";
 function App() {
   return (
     <div>
       <Header />
       <Searchbar />
       <About />
+<Footer />
     </div>
   );
 }
