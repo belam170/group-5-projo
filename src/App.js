b9a2d8fb144c6b58d3fce406449307ed63132e7b
belammuia <belammuia0@gmail.com>
2023-05-04 22:14:46 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './About';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './Nav';
 import Footer from './Footer';
-
+import ContactUs from './ContactUs';
 function App() {
   return (
     <div>
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/home" element={<Searchbar />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactUs />} />
         </Routes>
         <Footer />
       </Router>
